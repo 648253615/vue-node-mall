90cb185c91da19d1c7a7b92faf47eb9087551059
2301_79109125 <648253615@qq.com>
2023-09-11 17:42:20 +0800

    init

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,9 @@ module.exports = {
       host: ["8.134.154.68"],
       ref: "origin/main",
       repo: "git@github.com:648253615/vue-node-mall.git",
+      ssh_options: "StrictHostKeyChecking=no",
       path: "/home/vue-node-mall",
     }
   }
 }
+// pm2 deploy ecosystem.config.js production setup
