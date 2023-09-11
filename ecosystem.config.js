f7c3af41782a78fee7b7a9ec7245827170745682
2301_79109125 <648253615@qq.com>
2023-09-11 17:15:13 +0800

    init

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  deploy: {
+    production: {
+      user: "root",
+      host: ["8.134.154.68"],
+      ref: "origin/main",
+      repo: "git@github.com:648253615/vue-node-mall.git",
+      path: "/home/vue-node-mall",
+    }
+  }
+}
